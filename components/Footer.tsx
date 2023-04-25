5e535708e6664e5c8fe45b3d990c0bf0c4482f01
Heidiki <abdelattie@gmail.com>
2023-04-25 16:39:50 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import SocialIcons from '@/components/SocialIcons';
 import { memo } from 'react';
+import Link from 'next/link';
 
 function Footer(): React.ReactElement {
   const year: number = new Date().getFullYear();
@@ -7,8 +8,16 @@ function Footer(): React.ReactElement {
   return (
     <div className='mt-4 flex flex-col items-center py-8 font-light dark:text-white lg:py-8'>
       <SocialIcons />
-      <span className='mt-4 text-xs font-light lg:mt-8'>
-        &copy; {year} Karan Pratap Singh
+      <span className='mt-4 text-center text-xs font-light lg:mt-8'>
+        &copy; I forked this project from{' '}
+        <a
+          className='font-extrabold'
+          href='https://github.com/karanpratapsingh/portfolio'
+        >
+          karanpratapsingh/portfolio
+        </a>
+        <br />
+        Don't reinvent the wheel
       </span>
     </div>
   );
